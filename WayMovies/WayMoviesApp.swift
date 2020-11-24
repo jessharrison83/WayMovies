1375a630dae5b72ff1ce144546b9f9696114a9f2
Jess <jberler@wayfair.com>
2020-11-24 17:16:10 -0500

    Initial Commit

---
+++ WayMovies/WayMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WayMoviesApp.swift
+//  WayMovies
+//
+//  Created by Jessica Berler on 11/24/20.
+//
+
+import SwiftUI
+
+@main
+struct WayMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
